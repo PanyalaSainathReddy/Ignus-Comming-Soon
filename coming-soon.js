3217338dc6cc9cd4a51be81d27ec9fe5e9cfa2d9
PanyalaSainathReddy <reddy.17@iitj.ac.in>
2022-10-07 18:47:55 +0530

    Updates animation

--- coming-soon.js
+++ coming-soon.js
@@ -4,10 +4,11 @@ window.addEventListener("load", () => {
   var video_background = document.getElementById("video_background");
   var video_loading = document.getElementById("video-loading");
   var enter_video_div = document.getElementById("enter_video_div");
-  var bg_image = document.getElementById("bg_image");
+  // var bg_image = document.getElementById("bg_image");
+  var bg_video = document.getElementById("bg_video");
   const spotlight = document.querySelector(".spotlight");
   const audio = new Audio("static/videos/aud.mp4");
-  video_background.playbackRate = 7.5;
+  video_background.playbackRate = 1;
 
   enter_video_div.onclick = function () {
     enter_video_div.style.display = "none";
@@ -23,12 +24,14 @@ window.addEventListener("load", () => {
       video_background.style.display = "none";
       spotlight.style.display = "block";
       body.style.cursor = "none";
-      bg_image.style.display = "block";
+      bg_video.style.display = "block";
+      bg_video.play();
+      setTimeout(updateSpotlightInitial(), 1000);
     },
     false
   );
 
-  let spotlightSize = "transparent 20%, rgba(0, 0, 0, 0.9) 30%";
+  let spotlightSize = "transparent 20%, rgba(0, 0, 0, 0.8) 30%";
 
   window.addEventListener("mousemove", (e) => updateSpotlight(e));
   window.addEventListener("mousedown", (e) => {
@@ -37,7 +40,7 @@ window.addEventListener("load", () => {
   });
 
   window.addEventListener("mouseup", (e) => {
-    spotlightSize = "transparent 20%, rgba(0, 0, 0, 0.9) 30%";
+    spotlightSize = "transparent 20%, rgba(0, 0, 0, 0.8) 30%";
     updateSpotlight(e);
   });
 
@@ -45,5 +48,9 @@ window.addEventListener("load", () => {
     spotlight.style.backgroundImage = `radial-gradient(circle at ${
       (e.pageX / window.innerWidth) * 100
     }% ${(e.pageY / window.innerHeight) * 100}%, ${spotlightSize}`;
+  }
+
+  function updateSpotlightInitial() {
+    spotlight.style.backgroundImage = `radial-gradient(circle at ${50}% ${50}%, ${spotlightSize}`;
   }
 });
